0680aade66d04a883281922ca86bb23e8c177544
PROgrm_JARvis <mrjarviscraft@gmail.com>
2019-07-08 13:12:30 +0300

    Add `@DontOverrideEqualsAndHashCode` originally created in `minecraft-utils`

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/annotation/DontOverrideEqualsAndHashCode.java
@@ -0,0 +1,22 @@
+package ru.progrm_jarvis.javacommons.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Marker indicating that {@link Object#equals(Object)} and {@link Object#hashCode()} methods
+ * are not overridden for this class for some reason.
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target(value = ElementType.TYPE)
+public @interface DontOverrideEqualsAndHashCode {
+
+    /**
+     * Retrieves the reason why this class's {@link Object#equals(Object)} and {@link Object#hashCode()}
+     * methods are not overridden.
+     *
+     * @return reason for not overriding {@link Object#hashCode()} and {@link Object#equals(Object)} methods
+     */
+    String value() default "";
+}
