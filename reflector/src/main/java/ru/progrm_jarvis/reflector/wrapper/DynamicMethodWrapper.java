4c6bcb07033af11fb669bb3d8cf79dc9df066e81
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-07-24 18:54:58 +0300

    Fix wrong signature of `DynamicMethodWrapper#invoke(..)`

--- reflector/src/main/java/ru/progrm_jarvis/reflector/wrapper/DynamicMethodWrapper.java
+++ reflector/src/main/java/ru/progrm_jarvis/reflector/wrapper/DynamicMethodWrapper.java
@@ -4,5 +4,5 @@ import org.jetbrains.annotations.NotNull;
 
 public interface DynamicMethodWrapper<T, R> extends MethodWrapper<T, R> {
 
-    R invoke(@NotNull T target, @NotNull T... parameters);
+    R invoke(@NotNull T target, @NotNull Object... parameters);
 }
