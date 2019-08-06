9f782c577702fcfdceae74aacb9075d5c63c1d9c
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-08-06 20:44:26 +0300

    Add No-javassist implementation of `...createImmutableEnumSet(..)`

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/collection/CollectionFactory.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/collection/CollectionFactory.java
@@ -9,17 +9,16 @@ import lombok.experimental.UtilityClass;
 import lombok.val;
 import lombok.var;
 import org.intellij.lang.annotations.Language;
+import ru.progrm_jarvis.javacommons.bytecode.BytecodeLibrary;
 import ru.progrm_jarvis.javacommons.classload.ClassFactory;
 import ru.progrm_jarvis.javacommons.lazy.Lazy;
 import ru.progrm_jarvis.javacommons.util.ClassNamingStrategy;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static java.lang.Integer.getInteger;
+import static java.util.Collections.unmodifiableSet;
 
 /**
  * Factory used for creation of special {@link java.util.Collection collections} at classload.
@@ -149,81 +148,82 @@ public class CollectionFactory {
     public <E extends Enum<E>> Set<E> createImmutableEnumSet(@NonNull final E... values) {
         if (values.length == 0) return Collections.emptySet();
 
-        // sort enum values so that the cache does not store different instances for different orders of same
-        // elements
-        //noinspection unchecked,SuspiciousToArrayCall
-        val enumValues = (E[]) Arrays.stream(values)
-                .distinct()
-                .sorted()
-                .toArray(Enum[]::new);
+        if (BytecodeLibrary.JAVASSIST.isAvailable()) {
+            // sort enum values so that the cache does not store different instances for different orders of same
+            // elements
+            //noinspection unchecked,SuspiciousToArrayCall
+            val enumValues = (E[]) Arrays.stream(values)
+                    .distinct()
+                    .sorted()
+                    .toArray(Enum[]::new);
 
-        //noinspection unchecked
-        return (Set<E>) IMMUTABLE_ENUM_SETS.get(enumValues, () -> {
-            //<editor-fold desc="Class generation" defaultstate="collapsed">
-            val enumType = values.getClass().getComponentType();
-            // name of the class of the enum
-            val enumTypeName = enumType.getCanonicalName();
-            // number of enum constants
-            val elementsCount = enumValues.length;
-            // class pool instance
-            val classPool = CLASS_POOL.get();
-            // generated class
-            val clazz = classPool.makeClass(CLASS_NAMING_STRATEGY.get());
-            clazz.setSuperclass(ABSTRACT_IMMUTABLE_SET_CT_CLASS.get());
-            addEmptyConstructor(clazz);
+            //noinspection unchecked
+            return (Set<E>) IMMUTABLE_ENUM_SETS.get(enumValues, () -> {
+                //<editor-fold desc="Class generation" defaultstate="collapsed">
+                val enumType = values.getClass().getComponentType();
+                // name of the class of the enum
+                val enumTypeName = enumType.getCanonicalName();
+                // number of enum constants
+                val elementsCount = enumValues.length;
+                // class pool instance
+                val classPool = CLASS_POOL.get();
+                // generated class
+                val clazz = classPool.makeClass(CLASS_NAMING_STRATEGY.get());
+                clazz.setSuperclass(ABSTRACT_IMMUTABLE_SET_CT_CLASS.get());
+                addEmptyConstructor(clazz);
 
-            /* Generated methods */
+                /* Generated methods */
 
-            // `boolean contains(Object)` method
-            {
-                val src = new StringBuilder("public boolean contains(Object element) {");
-                // simply check type if any enum values is included
-                if (Arrays.equals(enumValues, enumType.getEnumConstants())) src
-                        .append("return element instanceof ").append(enumTypeName).append(';');
-                    // use ordinal switch if not all enum values are included
-                else {
-                    src
-                            .append("if (element instanceof ").append(enumTypeName).append(")")
-                            .append("switch (((Enum) element).ordinal()) {");
-
-                    for (val enumValue : enumValues)
+                // `boolean contains(Object)` method
+                {
+                    val src = new StringBuilder("public boolean contains(Object element) {");
+                    // simply check type if any enum values is included
+                    if (Arrays.equals(enumValues, enumType.getEnumConstants())) src
+                            .append("return element instanceof ").append(enumTypeName).append(';');
+                        // use ordinal switch if not all enum values are included
+                    else {
                         src
-                                .append("case ").append(enumValue.ordinal()).append(": return true;");
+                                .append("if (element instanceof ").append(enumTypeName).append(")")
+                                .append("switch (((Enum) element).ordinal()) {");
 
-                    src.append("default: return false;").append("}").append("return false;");
+                        for (val enumValue : enumValues)
+                            src
+                                    .append("case ").append(enumValue.ordinal()).append(": return true;");
+
+                        src.append("default: return false;").append("}").append("return false;");
+                    }
+                    addCtMethod(src.append('}').toString(), clazz);
                 }
-                addCtMethod(src.append('}').toString(), clazz);
-            }
 
-            // `boolean isEmpty()` method
-            addCtMethod(
-                    "public boolean isEmpty() {return false;}",
-                    clazz
-            );
-
-            // `int size()` method
-            addCtMethod(
-                    "public int size() {return " + elementsCount + ";}",
-                    clazz
-            );
-
-
-            val enumNames = Arrays.stream(enumValues)
-                    .map(element -> enumTypeName + '.' + element.name())
-                    .toArray(String[]::new);
-            { // field for storing array of elements and `toArray` methods
-                val newArrayStatement = "new " + enumTypeName + "[]{" + String.join(",", enumNames) + '}';
-
-                // add field with maximal accessibility so that it can be JITed as much as possible
-                addCtField(
-                        "public static final " + enumTypeName + "[] ARRAY_DATA = " + newArrayStatement + ';', clazz
-                );
-
-                // non-generic `toArray`
+                // `boolean isEmpty()` method
                 addCtMethod(
-                        "public Object[] toArray() {return " + newArrayStatement + ";}",
+                        "public boolean isEmpty() {return false;}",
                         clazz
                 );
+
+                // `int size()` method
+                addCtMethod(
+                        "public int size() {return " + elementsCount + ";}",
+                        clazz
+                );
+
+
+                val enumNames = Arrays.stream(enumValues)
+                        .map(element -> enumTypeName + '.' + element.name())
+                        .toArray(String[]::new);
+                { // field for storing array of elements and `toArray` methods
+                    val newArrayStatement = "new " + enumTypeName + "[]{" + String.join(",", enumNames) + '}';
+
+                    // add field with maximal accessibility so that it can be JITed as much as possible
+                    addCtField(
+                            "public static final " + enumTypeName + "[] ARRAY_DATA = " + newArrayStatement + ';', clazz
+                    );
+
+                    // non-generic `toArray`
+                    addCtMethod(
+                            "public Object[] toArray() {return " + newArrayStatement + ";}",
+                            clazz
+                    );
                 /* Not following the contract
                 addCtMethod(""
                                 + "public <T> T[] toArray(final T[] target) {"
@@ -236,84 +236,95 @@ public class CollectionFactory {
                         clazz
                 );
                  */
-                // generic `toArray`
-                // don't forget that javassist is not friendly with those and so use manual type erasure
-                addCtMethod(
-                        "public Object[] toArray(Object[] target) {"
-                                + "if (target.length < " + elementsCount + ") return java.util.Arrays.copyOf("
-                                + "ARRAY_DATA, " + elementsCount + ", target.getClass()"
-                                + ");"
-                                + "System.arraycopy(ARRAY_DATA, 0, target, 0, " + elementsCount + ");"
-                                + "if (target.length > " + elementsCount + ") target[" + elementsCount + "] = null;"
-                                + "return target;"
-                                + "}",
-                        clazz
-                );
-            }
+                    // generic `toArray`
+                    // don't forget that javassist is not friendly with those and so use manual type erasure
+                    addCtMethod(
+                            "public Object[] toArray(Object[] target) {"
+                                    + "if (target.length < " + elementsCount + ") return java.util.Arrays.copyOf("
+                                    + "ARRAY_DATA, " + elementsCount + ", target.getClass()"
+                                    + ");"
+                                    + "System.arraycopy(ARRAY_DATA, 0, target, 0, " + elementsCount + ");"
+                                    + "if (target.length > " + elementsCount + ") target[" + elementsCount + "] = null;"
+                                    + "return target;"
+                                    + "}",
+                            clazz
+                    );
+                }
 
-            {// `void forEach(Consumer)`
-                val src = new StringBuilder(
-                        "public void forEach(java.util.function.Consumer consumer) {"
-                        + "if (consumer == null) throw new NullPointerException(\"consumer should not be empty\");"
-                );
-                for (val enumName : enumNames) src.append("consumer.accept(").append(enumName).append(')').append(';');
+                {// `void forEach(Consumer)`
+                    val src = new StringBuilder(
+                            "public void forEach(java.util.function.Consumer consumer) {"
+                                    + "if (consumer == null) throw new NullPointerException(\"consumer should not be empty\");"
+                    );
+                    for (val enumName : enumNames) src.append("consumer.accept(").append(enumName).append(')').append(';');
 
-                addCtMethod(src.append('}').toString(), clazz);
-            }
-            // `hashCode()`
-            addCtMethod("int hashCode() {return " + Arrays.hashCode(values) + ";}", clazz);
-            // `equals()`
-            {// `boolean equals(Object)`
-                addCtMethod(
-                        "public boolean equals(Object object) {"
-                                + "if (object == this) return true;"
-                                + "if (!(object instanceof java.util.Collection)) return false;"
-                                + "java.util.Collection set =  (java.util.Collection) object;"
-                                + "if (set.size() != " + elementsCount + ") return false;"
-                                + "return set.containsAll(this);"
-                                + "}",
-                        clazz
-                );
-            }
+                    addCtMethod(src.append('}').toString(), clazz);
+                }
+                // `hashCode()`
+                addCtMethod("int hashCode() {return " + Arrays.hashCode(values) + ";}", clazz);
+                // `equals()`
+                {// `boolean equals(Object)`
+                    addCtMethod(
+                            "public boolean equals(Object object) {"
+                                    + "if (object == this) return true;"
+                                    + "if (!(object instanceof java.util.Collection)) return false;"
+                                    + "java.util.Collection set =  (java.util.Collection) object;"
+                                    + "if (set.size() != " + elementsCount + ") return false;"
+                                    + "return set.containsAll(this);"
+                                    + "}",
+                            clazz
+                    );
+                }
 
             /*
             Generate iterator
              */
-            final CtClass iteratorClazz;
-            {
-                iteratorClazz = clazz.makeNestedClass(CLASS_NAMING_STRATEGY.get(), true);
-                iteratorClazz.setInterfaces(ITERATOR_CT_CLASS_ARRAY.get());
-                addEmptyConstructor(iteratorClazz);
-
-                addCtField("public int index = 0;", iteratorClazz);
-                addCtMethod(
-                        "public java.util.Iterator iterator() {"
-                                + "return new " + iteratorClazz.getName() + "();"
-                                + "}",
-                        clazz
-                );
-
-                // boolean hasNext()
-                addCtMethod("public boolean hasNext() {return index < " + elementsCount + ";}", iteratorClazz);
+                final CtClass iteratorClazz;
                 {
-                    // T next()
-                    final StringBuilder src = new StringBuilder("public Object next() {switch (index++) {");
+                    iteratorClazz = clazz.makeNestedClass(CLASS_NAMING_STRATEGY.get(), true);
+                    iteratorClazz.setInterfaces(ITERATOR_CT_CLASS_ARRAY.get());
+                    addEmptyConstructor(iteratorClazz);
 
-                    for (var i = 0; i < elementsCount; i++)
-                        src
-                                .append("case ").append(i).append(':').append("return ").append(enumNames[i])
-                                .append(';');
-
+                    addCtField("public int index = 0;", iteratorClazz);
                     addCtMethod(
-                            src.append("default: throw new java.util.NoSuchElementException();}}")
-                                    .toString(),
-                            iteratorClazz
+                            "public java.util.Iterator iterator() {"
+                                    + "return new " + iteratorClazz.getName() + "();"
+                                    + "}",
+                            clazz
                     );
-                }
-            }
 
-            return (Set<Enum<?>>) ClassFactory.defineGCClasses(iteratorClazz, clazz)[1].newInstance();
-            //</editor-fold>
-        });
+                    // boolean hasNext()
+                    addCtMethod("public boolean hasNext() {return index < " + elementsCount + ";}", iteratorClazz);
+                    {
+                        // T next()
+                        final StringBuilder src = new StringBuilder("public Object next() {switch (index++) {");
+
+                        for (var i = 0; i < elementsCount; i++)
+                            src
+                                    .append("case ").append(i).append(':').append("return ").append(enumNames[i])
+                                    .append(';');
+
+                        addCtMethod(
+                                src.append("default: throw new java.util.NoSuchElementException();}}")
+                                        .toString(),
+                                iteratorClazz
+                        );
+                    }
+                }
+
+                return (Set<Enum<?>>) ClassFactory.defineGCClasses(iteratorClazz, clazz)[1].newInstance();
+                //</editor-fold>
+            });
+        } else {
+            val length = values.length;
+            switch (length) {
+                case 1: return unmodifiableSet(EnumSet.of(values[0]));
+                case 2: return unmodifiableSet(EnumSet.of(values[0], values[1]));
+                case 3: return unmodifiableSet(EnumSet.of(values[0], values[1], values[2]));
+                case 4: return unmodifiableSet(EnumSet.of(values[0], values[1], values[2], values[3]));
+                case 5: return unmodifiableSet(EnumSet.of(values[0], values[1], values[2], values[3], values[4]));
+                default: return unmodifiableSet(EnumSet.copyOf(Arrays.asList(values)));
+            }
+        }
     }
 }
