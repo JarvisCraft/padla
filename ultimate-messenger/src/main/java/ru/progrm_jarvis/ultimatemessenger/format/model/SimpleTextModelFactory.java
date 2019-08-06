e6c011f5ddb17a1fc316f133f6219dc38dae5ed9
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-08-06 19:17:56 +0300

    Make TextModelTemplate child of AbstractCachingTextModelFactoryTemplate

--- ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/SimpleTextModelFactory.java
+++ ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/SimpleTextModelFactory.java
@@ -25,19 +25,23 @@ public class SimpleTextModelFactory<T> implements TextModelFactory<T> {
      * @param <T> type of object according to which the created text models are formatted
      */
     @ToString
-    @EqualsAndHashCode // simply, why not? :)
+    @EqualsAndHashCode(callSuper = true) // simply, why not? :) (this also allows instance caching)
     @FieldDefaults(level = AccessLevel.PROTECTED, makeFinal = true)
-    protected static class TextModelTemplate<T> implements TextModelFactory.TextModelTemplate<T> {
+    protected static class TextModelTemplate<T> extends AbstractCachingTextModelFactoryTemplate<T> {
 
-        List<TextModel<T>> elements = new ArrayList<>();
+        @NonNull List<TextModel<T>> elements = new ArrayList<>();
 
         @NonFinal transient String lastStaticText;
 
         @Override
         public TextModelFactory.TextModelTemplate<T> append(@NonNull final String staticText) {
-            if (lastStaticText == null) elements.add(StaticTextModel.of(lastStaticText = staticText));
-            else elements.set(elements.size() - 1, StaticTextModel.of(lastStaticText += staticText)); // ...
-            // ... join nearby static text blocks
+            if (!staticText.isEmpty()) {
+                if (lastStaticText == null) elements.add(StaticTextModel.of(lastStaticText = staticText));
+                else elements.set(elements.size() - 1, StaticTextModel.of(lastStaticText += staticText)); // ...
+                // ... join nearby static text blocks
+
+                markAsChanged();
+            }
 
             return this;
         }
@@ -46,26 +50,26 @@ public class SimpleTextModelFactory<T> implements TextModelFactory<T> {
         public TextModelFactory.TextModelTemplate<T> append(@NonNull final TextModel<T> dynamicText) {
             elements.add(dynamicText);
             lastStaticText = null;
+            markAsChanged();
 
             return this;
         }
 
         @Override
         public TextModelFactory.TextModelTemplate<T> clear() {
-            elements.clear();
-            lastStaticText = null;
+            if (!elements.isEmpty()) {
+                elements.clear();
+                lastStaticText = null;
+
+                markAsChanged();
+            }
 
             return this;
         }
 
         @Override
-        public TextModel<T> create() {
+        protected TextModel<T> createTextModel(final boolean release) {
             return elements.isEmpty() ? TextModel.empty() : DelegatingNestingTextModel.fromCopyOf(elements);
-        }
-
-        @Override
-        public TextModel<T> createAndRelease() {
-            return elements.isEmpty() ? TextModel.empty() : DelegatingNestingTextModel.from(elements);
         }
     }
 }
