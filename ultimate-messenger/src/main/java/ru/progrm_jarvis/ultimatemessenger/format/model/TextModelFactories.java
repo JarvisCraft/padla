eab7424a9ae2fd10a841731b188976e9221c100d
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-08-06 20:34:31 +0300

    Add TextModelFactories

---
+++ ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/TextModelFactories.java
@@ -0,0 +1,27 @@
+package ru.progrm_jarvis.ultimatemessenger.format.model;
+
+import ru.progrm_jarvis.javacommons.bytecode.BytecodeLibrary;
+
+/**
+ * Utility making use of {@link TextModelFactory text model factories} easier.
+ */
+public class TextModelFactories {
+
+    /**
+     * Gets the best available {@link TextModelFactory text model factory}.
+     *
+     * @param <T> generic type of {@link TextModelFactory}
+     * @return the best available {@link TextModelFactory text model factory}
+     */
+    protected <T> TextModelFactory<T> getAvailable() {
+        if (BytecodeLibrary.ASM.isAvailable()) try {
+            return AsmGeneratingTextModelFactory.get();
+        } catch (final Throwable ignored) {}
+
+        if (BytecodeLibrary.JAVASSIST.isAvailable()) try {
+            return AsmGeneratingTextModelFactory.get();
+        } catch (final Throwable ignored) {}
+
+        return SimpleTextModelFactory.get();
+    }
+}
