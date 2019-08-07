fc4c253602eb0bad32c5783741825748b2c1c816
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-08-07 18:33:11 +0300

    Update docs of `TextModel` updating its `empty()`

--- ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/TextModel.java
+++ ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/TextModel.java
@@ -11,14 +11,54 @@ import org.jetbrains.annotations.NotNull;
 @FunctionalInterface
 public interface TextModel<T> {
 
+    /**
+     * Gets the text formatted for the given target.
+     *
+     * @param target object according to which the text models gets formatted
+     * @return text formatted for the given target
+     */
     @NotNull String getText(@NotNull T target);
 
+    /**
+     * Retrieves whether this {@link TextModel text model} is dynamic.
+     *
+     * @return {@code true} if this {@link TextModel text model} may produce different values
+     * and {@code false} if it produces equal values for all calls to {@link #getText(Object)}
+     */
     @Contract(pure = true)
     default boolean isDynamic() {
         return true;
     }
 
+    /**
+     * Returns an empty static {@link TextModel text model}.
+     *
+     * @param <T> type of object according to which the text model is formatted
+     * @return singleton of an empty static {@link TextModel text model}
+     */
+    @SuppressWarnings("unchecked")
     static <T> TextModel<T> empty() {
-        return target -> "";
+        return EmptyTextModel.INSTANCE;
+    }
+
+    /**
+     * Empty static {@link TextModel text model}.
+     */
+    final class EmptyTextModel implements TextModel {
+
+        /**
+         * Singleton instance of this {@link TextModel text model}
+         */
+        private static EmptyTextModel INSTANCE = new EmptyTextModel();
+
+        @Override
+        @NotNull public String getText(@NotNull final Object target) {
+            return "";
+        }
+
+        @Override
+        public boolean isDynamic() {
+            return false;
+        }
     }
 }
