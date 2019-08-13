d3aab21dd5a59416ece986fa7896201a7bf30faa
PROgrm_JARvis <mrjarviscraft@gmail.com>
2019-08-13 14:25:14 +0300

    Add `ValueStorage` API to use it for generated classes

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/valuestorage/ValueStorage.java
@@ -0,0 +1,29 @@
+package ru.progrm_jarvis.javacommons.util.valuestorage;
+
+import lombok.NonNull;
+
+/**
+ * Storage of values which is made for sharing values which is unavailable via direct standard APIs
+ * such as initializing static final fields of generated classes.
+ *
+ * @param <K> type of keys used for identifying values
+ * @param <V> type of values stored
+ */
+public interface ValueStorage<K, V> {
+
+    /**
+     * Stores the given non-null value returning its unique key.
+     *
+     * @param value non-null value to store
+     * @return unique key by which the stored value may be retrieved
+     */
+    K storeValue(@NonNull V value);
+
+    /**
+     * Retrieves (gets and removes) the value stored by the given key.
+     *
+     * @param key unique key by which the value should be found
+     * @return value which was stored by the given key or {@code null} if there was no value stored
+     */
+    V retrieveValue(K key);
+}
