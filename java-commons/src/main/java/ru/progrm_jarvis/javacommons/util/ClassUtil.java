405c8833e94c2f168979f110a056533443048c14
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2020-02-02 17:04:18 +0300

    Add `ClassUtil` with utility-methods for primitives and their wrappers

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/ClassUtil.java
@@ -0,0 +1,141 @@
+package ru.progrm_jarvis.javacommons.util;
+
+import com.google.common.base.Preconditions;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import lombok.var;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Utility for class-related stuff.
+ */
+@UtilityClass
+public class ClassUtil {
+
+    /**
+     * Comparator for sorting {@link Class classes} by their hash-code
+     */
+    private static final Comparator<Class<?>> CLASS_HASH_CODE_COMPARATOR = Comparator.comparing(Class::hashCode);
+
+    /* *************************************** Sorted by PROgrammer_JARvis :) *************************************** */
+    /**
+     * Array of primitive classes (those whose {@link Class#isPrimitive()} returns {@code true})
+     */
+    private static final Class<?>[] PRIMITIVE_CLASSES = new Class<?>[]{
+            boolean.class, byte.class, char.class, short.class,
+            int.class, long.class, float.class, double.class
+    },
+    /**
+     * Array of primitive-wrapper classes in the same order
+     * as the corresponding primitive classes in {@link #PRIMITIVE_CLASSES}
+     */
+    PRIMITIVE_WRAPPER_CLASSES = new Class<?>[]{
+            Boolean.class, Byte.class, Character.class, Short.class,
+            Integer.class, Long.class, Float.class, Double.class
+    },
+    /* ******************************************** Sorted by primitives ******************************************** */
+    /**
+     * Sorted variant of {@link #PRIMITIVE_CLASSES}
+     */
+    SORTED_PRIMITIVE_CLASSES,
+    /**
+     * Array of primitive-wrapper classes in the same order
+     * as the corresponding primitive classes in {@link #SORTED_PRIMITIVE_CLASSES}
+     */
+    PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES,
+    /* ********************************************* Sorted by wrappers ********************************************* */
+    /**
+     * Sorted variant of {@link #PRIMITIVE_WRAPPER_CLASSES}
+     */
+    SORTED_PRIMITIVE_WRAPPER_CLASSES,
+    /**
+     * Array of primitive classes in the same order
+     * as the corresponding primitive-wrapper classes in {@link #SORTED_PRIMITIVE_WRAPPER_CLASSES}
+     */
+    PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES;
+
+    static {
+        SORTED_PRIMITIVE_CLASSES = PRIMITIVE_CLASSES.clone();
+        Arrays.sort(SORTED_PRIMITIVE_CLASSES, CLASS_HASH_CODE_COMPARATOR);
+
+        PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES = new Class<?>[SORTED_PRIMITIVE_CLASSES.length];
+        for (var i = 0; i < SORTED_PRIMITIVE_CLASSES.length; i++) {
+            val type = SORTED_PRIMITIVE_CLASSES[i];
+
+            if (type == boolean.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Boolean.class;
+            else if (type == byte.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Byte.class;
+            else if (type == char.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Character.class;
+            else if (type == short.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Short.class;
+            else if (type == int.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Integer.class;
+            else if (type == long.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Long.class;
+            else if (type == float.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Float.class;
+            else if (type == double.class) PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[i] = Double.class;
+            else throw new Error("Unknown primitive class: " + type); // Failsafe, just in case
+        }
+
+        SORTED_PRIMITIVE_WRAPPER_CLASSES = PRIMITIVE_WRAPPER_CLASSES.clone();
+        Arrays.sort(SORTED_PRIMITIVE_WRAPPER_CLASSES, CLASS_HASH_CODE_COMPARATOR);
+
+        PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES = new Class<?>[SORTED_PRIMITIVE_WRAPPER_CLASSES.length];
+        for (var i = 0; i < SORTED_PRIMITIVE_WRAPPER_CLASSES.length; i++) {
+            val type = SORTED_PRIMITIVE_WRAPPER_CLASSES[i];
+
+            if (type == Boolean.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = boolean.class;
+            else if (type == Byte.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = byte.class;
+            else if (type == Character.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = char.class;
+            else if (type == Short.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = short.class;
+            else if (type == Integer.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = int.class;
+            else if (type == Long.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = long.class;
+            else if (type == Float.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = float.class;
+            else if (type == Double.class) PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[i] = double.class;
+            else throw new Error("Unknown primitive-wrapper class: " + type); // Failsafe, just in case
+        }
+    }
+
+    /**
+     * Checks if the given class is a primitive wrapper.
+     *
+     * @param clazz class to check
+     * @return {@code true} if the class is a primitive wrapper and {@code false} otherwise
+     */
+    public boolean isPrimitiveWrapper(final Class<?> clazz) {
+        return Arrays.binarySearch(SORTED_PRIMITIVE_WRAPPER_CLASSES, clazz, CLASS_HASH_CODE_COMPARATOR) >= 0;
+    }
+
+    /**
+     * Gets a primitive-wrapper class for the given primitive class.
+     *
+     * @param primitiveClass primitive class whose wrapper is needed
+     * @return primitive-wrapper class for the given primitive class
+     *
+     * @throws IllegalArgumentException if the given class is not primitive
+     */
+    public Class<?> toPrimitiveWrapper(@NotNull /* hot spot */ final Class<?> primitiveClass) {
+        val primitiveClassIndex = Arrays.binarySearch(
+                SORTED_PRIMITIVE_CLASSES, primitiveClass, CLASS_HASH_CODE_COMPARATOR
+        );
+        Preconditions.checkArgument(primitiveClassIndex >= 0, "Given class is not primitive");
+
+        return PRIMITIVE_WRAPPER_CLASSES_SORTED_BY_PRIMITIVE_CLASSES[primitiveClassIndex];
+    }
+
+    /**
+     * Gets a primitive class for the given primitive-wrapper class.
+     *
+     * @param primitiveWrapperClass primitive-wrapper class whose wrapper is needed
+     * @return primitive class for the given primitive-wrapper class
+     *
+     * @throws IllegalArgumentException if the given class is not a primitive-wrapper
+     */
+    public Class<?> toPrimitive(@NotNull /* hot spot */ final Class<?> primitiveWrapperClass) {
+        val primitiveClassIndex = Arrays.binarySearch(
+                SORTED_PRIMITIVE_WRAPPER_CLASSES, primitiveWrapperClass, CLASS_HASH_CODE_COMPARATOR
+        );
+        Preconditions.checkArgument(primitiveClassIndex >= 0, "Given class is not a primitive-wrapper");
+
+        return PRIMITIVE_CLASSES_SORTED_BY_PRIMITIVE_WRAPPER_CLASSES[primitiveClassIndex];
+    }
+}
---
+++ java-commons/src/test/java/ru/progrm_jarvis/javacommons/util/ClassUtilTest.java
@@ -0,0 +1,121 @@
+package ru.progrm_jarvis.javacommons.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.BitSet;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class ClassUtilTest {
+
+    protected static Stream<Arguments> providePrimitives() {
+        return Stream.of(
+                arguments(boolean.class),
+                arguments(byte.class),
+                arguments(char.class),
+                arguments(short.class),
+                arguments(int.class),
+                arguments(long.class),
+                arguments(float.class),
+                arguments(double.class)
+        );
+    }
+
+    protected static Stream<Arguments> providePrimitiveWrappers() {
+        return Stream.of(
+                arguments(Boolean.class),
+                arguments(Byte.class),
+                arguments(Character.class),
+                arguments(Short.class),
+                arguments(Integer.class),
+                arguments(Long.class),
+                arguments(Float.class),
+                arguments(Double.class)
+        );
+    }
+
+    protected static Stream<Arguments> provideNonPrimitiveOrWrapperClasses() {
+        return Stream.of(
+                arguments(Object.class),
+                arguments(String.class),
+                arguments(BigInteger.class),
+                arguments(BigDecimal.class),
+                arguments(BitSet.class),
+                arguments(ClassUtilTest.class)
+        );
+    }
+
+    protected static Stream<Arguments> providePrimitivesToPrimitiveWrappers() {
+        return Stream.of(
+                arguments(boolean.class, Boolean.class),
+                arguments(byte.class, Byte.class),
+                arguments(char.class, Character.class),
+                arguments(short.class, Short.class),
+                arguments(int.class, Integer.class),
+                arguments(long.class, Long.class),
+                arguments(float.class, Float.class),
+                arguments(double.class, Double.class)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitiveWrappers")
+    void testIsPrimitiveWrapperWithPrimitiveWrappers(@NotNull final Class<?> primitiveWrapper) {
+        assertTrue(ClassUtil.isPrimitiveWrapper(primitiveWrapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitives")
+    void testIsPrimitiveWrapperWithPrimitives(@NotNull final Class<?> primitive) {
+        assertFalse(ClassUtil.isPrimitiveWrapper(primitive));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNonPrimitiveOrWrapperClasses")
+    void testIsPrimitiveWrapperWithNonPrimitiveOrWrapperClasses(@NotNull final Class<?> nonPrimitiveOrWrapper) {
+        assertFalse(ClassUtil.isPrimitiveWrapper(nonPrimitiveOrWrapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitivesToPrimitiveWrappers")
+    void testToPrimitiveWrapperWithValid(@NotNull final Class<?> primitive, @NotNull final Class<?> wrapper) {
+        assertSame(wrapper, ClassUtil.toPrimitiveWrapper(primitive));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitiveWrappers")
+    void testToPrimitiveWrapperWithPrimitiveWrappers( @NotNull final Class<?> wrapper) {
+        assertThrows(IllegalArgumentException.class, () -> ClassUtil.toPrimitiveWrapper(wrapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNonPrimitiveOrWrapperClasses")
+    void testToPrimitiveWrapperWithNonPrimitiveOrWrapperClasses(@NotNull final Class<?> nonPrimitiveOrWrapper) {
+        assertThrows(IllegalArgumentException.class, () -> ClassUtil.toPrimitiveWrapper(nonPrimitiveOrWrapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitivesToPrimitiveWrappers")
+    void testToPrimitiveWithValid(@NotNull final Class<?> primitive, @NotNull final Class<?> wrapper) {
+        assertSame(primitive, ClassUtil.toPrimitive(wrapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePrimitives")
+    void testToPrimitiveWithPrimitives(@NotNull final Class<?> primitive) {
+        assertThrows(IllegalArgumentException.class, () -> ClassUtil.toPrimitive(primitive));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNonPrimitiveOrWrapperClasses")
+    void testToPrimitiveWithNonPrimitiveOrWrapperClasses(@NotNull final Class<?> unrelated) {
+        assertThrows(IllegalArgumentException.class, () -> ClassUtil.toPrimitive(unrelated));
+    }
+}
