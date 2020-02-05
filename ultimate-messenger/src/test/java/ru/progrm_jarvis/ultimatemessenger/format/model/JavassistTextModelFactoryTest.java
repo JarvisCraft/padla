130b6fb374ed6efcd36ab81c7d9671d76ed15b1f
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2020-02-05 23:06:36 +0300

    Limit JavassistTextModelFactoryTest to Java 9+ due to IsolatedClassLoader issues with Surefire

--- ultimate-messenger/src/test/java/ru/progrm_jarvis/ultimatemessenger/format/model/JavassistTextModelFactoryTest.java
+++ ultimate-messenger/src/test/java/ru/progrm_jarvis/ultimatemessenger/format/model/JavassistTextModelFactoryTest.java
@@ -1,7 +1,11 @@
 package ru.progrm_jarvis.ultimatemessenger.format.model;
 
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+
 import java.util.stream.Stream;
 
+@EnabledForJreRange(min = JRE.JAVA_9) // Surefire uses IsolatedClassLoader which causes issues with ClassPool
 class JavassistTextModelFactoryTest extends AbstractTextModelFactoryTest {
 
     static Stream<TextModelFactory<User>> provideTestSubjects() {
