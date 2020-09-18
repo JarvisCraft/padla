56bb354ac1fe179b25a495ea913cbcc4048d63e3
progrm_jarvis <gh@progrm-jarvis.ru>
2020-09-18 21:55:33 +0300

    feat: add ownership annotations

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/annotation/ownership/Own.java
@@ -0,0 +1,23 @@
+package ru.progrm_jarvis.javacommons.annotation.ownership;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>Marker indicating the owner of a mutable object.
+ * It is intended only for use with mutable types
+ * for which it will indicate whether or not the operated object should be cloned for modification</p>
+ * <p>Owner depends on the target:</p>
+ * <dl>
+ *     <dt>parameter</dt>
+ *     <dd>method whose parameter it is</dd>
+ *
+ *     <dt>return type</dt>
+ *     <dd>caller of the method accepting the returned value</dd>
+ * </dl>
+ *
+ * @see Ref counterpart
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS) // keep even if sources are unavailable
+@Target({ElementType.TYPE_USE /* to allow on return type */})
+public @interface Own {}
---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/annotation/ownership/Ref.java
@@ -0,0 +1,23 @@
+package ru.progrm_jarvis.javacommons.annotation.ownership;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>Marker indicating the non-owner of a mutable object.
+ * It is intended only for use with mutable types
+ * for which it will indicate whether or not the operated object should be cloned for modification</p>
+ * <p>Owner depends on the target:</p>
+ * <dl>
+ *     <dt>parameter</dt>
+ *     <dd>caller of the method whose parameter it is</dd>
+ *
+ *     <dt>return type</dt>
+ *     <dd>method whose returned value it is</dd>
+ * </dl>
+ *
+ * @see Own counterpart
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS) // keep even if sources are unavailable
+@Target({ElementType.TYPE_USE /* to allow on return type */})
+public @interface Ref {}
