f1c731ad8a056b7cf56afaee3798b0a149bb25cd
progrm_jarvis <gh@progrm-jarvis.ru>
2020-11-28 21:10:51 +0300

    fix: use correct factory in JavassistTextModelFactoryTest

--- ultimate-messenger/src/test/java/ru/progrm_jarvis/ultimatemessenger/format/model/JavassistTextModelFactoryTest.java
+++ ultimate-messenger/src/test/java/ru/progrm_jarvis/ultimatemessenger/format/model/JavassistTextModelFactoryTest.java
@@ -2,13 +2,12 @@ package ru.progrm_jarvis.ultimatemessenger.format.model;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.invoke.MethodHandles;
 import java.util.stream.Stream;
 
 class JavassistTextModelFactoryTest extends AbstractTextModelFactoryTest {
 
     @Override
     protected @NotNull Stream<@NotNull TextModelFactory<@NotNull User>> provideTestSubjects() {
-        return Stream.of(JavassistTextModelFactory.create(MethodHandles.lookup()));
+        return Stream.of(JavassistTextModelFactory.create());
     }
 }
