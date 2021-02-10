ab447a027090298f35137eca03b7a99f2a4d74ab
Petr Portnov <gh@progrm-jarvis.ru>
2021-02-10 19:35:43 +0300

    docs: fix docs of package-info of object.extension package

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/object/extension/package-info.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/object/extension/package-info.java
@@ -1,6 +1,5 @@
 /**
  * Common extensions to be used with
- * {@link 'https://projectlombok.org/features/experimental/ExtensionMethod''}
  * <a href="https://projectlombok.org/features/experimental/ExtensionMethod">Lombok's ExtensionMethod feature</a>
  */
 package ru.progrm_jarvis.javacommons.object.extension;
