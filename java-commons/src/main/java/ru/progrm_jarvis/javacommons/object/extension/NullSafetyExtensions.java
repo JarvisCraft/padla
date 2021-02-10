6d8314b7672cc37a5fcdb9469f09837013b2ea6c
Petr Portnov <gh@progrm-jarvis.ru>
2021-02-10 19:33:25 +0300

    feat: implement NullSafetyExtensions

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/object/extension/NullSafetyExtensions.java
@@ -0,0 +1,164 @@
+package ru.progrm_jarvis.javacommons.object.extension;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Extensions to provide null-safety on nullable types.
+ * These methods behave specifically, for example call on {@code null} may not throw {@link NullPointerException}
+ * thus they have underscores ({@code _}) in their names.
+ */
+@UtilityClass
+public class NullSafetyExtensions {
+
+    /**
+     * Returns the passed value if it not {@code null} otherwise returning the default one.
+     *
+     * @param value value whose nullability is tested
+     * @param defaultValue value returned if {@code value} is {@code null}
+     * @param <T> type of the value
+     * @return {@code value} if it is not {@code null} and {@code defaultValue} otherwise
+     */
+    @Contract("!null, _ -> param1; null, _ -> param2")
+    public <T> T _or(final @Nullable T value,
+                     final @Nullable T defaultValue) {
+        return value == null ? defaultValue : value;
+    }
+
+    /**
+     * Returns the passed value if it not {@code null} otherwise returning the default one.
+     *
+     * @param value value whose nullability is tested
+     * @param defaultValueSupplier supplier of the value returned if {@code value} is {@code null}
+     * @param <T> type of the value
+     * @return {@code value} if it is not {@code null} and {@code defaultValue} otherwise
+     *
+     * @throws NullPointerException if {@code defaultValueSupplier} is {@code null}
+     */
+    @SuppressWarnings("Contract") // Lombok's annotation is treated incorrectly
+    @Contract("_, null -> fail; !null, _ -> param1; null, _ -> _")
+    public <T> T _orGet(final @Nullable T value,
+                        final @NonNull Supplier<@Nullable ? extends T> defaultValueSupplier) {
+        return value == null ? defaultValueSupplier.get() : value;
+    }
+
+    /**
+     * Maps the given value if it is {@code null} otherwise returning {@code null}.
+     *
+     * @param value value to be mapped if it is not {@code null}
+     * @param mappingFunction function to be used for mapping of the the non-{@code null} value
+     * @param <T> type of the source value
+     * @param <R> type of the mapped value
+     * @return mapped {@code value} if the latter was not {@code null} or {@code null} otherwise
+     *
+     * @throws NullPointerException if {@code mappingFunction} is {@code null}
+     */
+    @SuppressWarnings("Contract") // Lombok's annotation is treated incorrectly
+    @Contract("_, null -> fail; !null, _ -> _; null, _ -> null")
+    public <T, R> @Nullable R _map(final @Nullable T value,
+                                   final @NonNull Function<@NotNull T, R> mappingFunction) {
+        return value == null ? null : mappingFunction.apply(value);
+    }
+
+    /**
+     * Maps the given value.
+     *
+     * @param value value to be mapped
+     * @param mappingFunction function to be used for mapping of the value
+     * @param <T> type of the source value
+     * @param <R> type of the mapped value
+     * @return mapped value
+     *
+     * @throws NullPointerException if {@code mappingFunction} is {@code null}
+     */
+    @SuppressWarnings("Contract") // Lombok's annotation is treated incorrectly
+    @Contract("_, null -> fail; _, _ -> _")
+    public <T, R> @Nullable R _mapNullable(final @Nullable T value,
+                                           final @NonNull Function<@Nullable T, R> mappingFunction) {
+        return mappingFunction.apply(value);
+    }
+
+    /**
+     * Returns the passed value if it is not {@code null} and matches the predicate and {@code null} otherwise.
+     *
+     * @param value tested value
+     * @param predicate condition to test the value agains
+     * @param <T> type of the value
+     * @return {@code value} if it is not {@code null} and matches the predicate and {@code null} otherwise.
+     *
+     * @throws NullPointerException if {@code predicate} is {@code null}
+     */
+    @SuppressWarnings("Contract") // Lombok's annotation is treated incorrectly
+    @Contract("_, null -> fail; !null, _ -> _; null, _ -> null")
+    public <T> @Nullable T _filter(final @Nullable T value,
+                                          final @NonNull Predicate<T> predicate) {
+        return value == null ? null : predicate.test(value) ? value : null;
+    }
+
+    /**
+     * Returns the passed value if it matches the predicate and {@code null} otherwise.
+     *
+     * @param value tested value
+     * @param predicate condition to test the value agains
+     * @param <T> type of the value
+     * @return {@code value} if it matches the predicate and {@code null} otherwise.
+     *
+     * @throws NullPointerException if {@code predicate} is {@code null}
+     */
+    @SuppressWarnings("Contract") // Lombok's annotation is treated incorrectly
+    @Contract("_, null -> fail; _, _ -> _")
+    public <T> @Nullable T _filterNullable(final @Nullable T value, final @NonNull Predicate<@Nullable T> predicate) {
+        return predicate.test(value) ? value : null;
+    }
+
+    /**
+     * Throws the supplied exception if the given value is null.
+     *
+     * @param value checked value
+     * @param throwableSupplier supplier of a throwable used when the value is {@code null}
+     * @param <T> type of the value
+     * @param <X> thrown typethrowable
+     * @return {@code value} if it is not {@code null}
+     * @throws X if {@code value} is null
+     * @throws NullPointerException if {@code throwableSupplier} or it supplies {@code null}
+     */
+    public <T, X extends Throwable> @NotNull T _orElseThrow(final @Nullable T value,
+                                                            final @NonNull Supplier<@NonNull X>
+                                                                    throwableSupplier) throws X {
+        if (value == null) throw throwableSupplier.get();
+
+        return value;
+    }
+
+    /**
+     * Converts the given value into a {@link Stream stream} containing the value
+     * if is not {@code null} or an {@link Stream#empty() empty stream} otherwise.
+     *
+     * @param value value to be converted into a stream if it is not {@code null}
+     * @param <T> type of the value
+     * @return {@link Stream stream} containing the value if it is not {@code null}
+     * or an {@link Stream#empty() empty stream} otherwise.
+     */
+    public <T> @NotNull Stream<@NotNull T> _stream(final @Nullable T value) {
+        return value == null ? Stream.empty() : Stream.of(value);
+    }
+
+    /**
+     * Converts the given value into a {@link Stream stream}.
+     *
+     * @param value value to be converted into a stream if it is not {@code null}
+     * @param <T> type of the value
+     * @return {@link Stream stream} containing the value.
+     */
+    public <T> @NotNull Stream<@Nullable T> _streamOfNullable(final @Nullable T value) {
+        return Stream.of(value);
+    }
+}
---
+++ java-commons/src/test/java/ru/progrm_jarvis/javacommons/object/extension/NullSafetyExtensionsTest.java
@@ -0,0 +1,132 @@
+package ru.progrm_jarvis.javacommons.object.extension;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.ExtensionMethod;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtensionMethod(NullSafetyExtensions.class)
+class NullSafetyExtensionsTest {
+
+    private static final @NotNull String @NotNull [] EMPTY_STRING_ARRAY = {};
+
+    @Test
+    void _or() {
+        assertEquals("foo", "foo"._or("bar"));
+        assertEquals("qux", "qux"._or(null));
+        assertEquals("baz", ((String) null)._or("baz"));
+        assertNull(((String) null)._or(null));
+    }
+
+    @Test
+    void _orGet() {
+        assertEquals("foo", "foo"._orGet(Assertions::fail));
+        assertEquals("baz", ((String) null)._orGet(() -> "baz"));
+        assertNull(((String) null)._orGet(() -> null));
+    }
+
+    @Test
+    void _orGet_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "magic"._orGet(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._orGet(null));
+    }
+
+    @Test
+    void _map() {
+        assertEquals("abcd", "ab"._map(value -> {
+            assertEquals("ab", value);
+            return value + "cd";
+        }));
+        assertNull("yep"._map(value -> {
+            assertEquals("yep", value);
+            return null;
+        }));
+        assertNull(((String) null)._map(value -> fail()));
+    }
+
+    @Test
+    void _map_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "smth"._map(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._map(null));
+    }
+
+    @Test
+    void _mapNullable() {
+        assertEquals("abcd", "ab"._mapNullable(value -> value + "cd"));
+        assertNull("yep"._mapNullable(value -> null));
+        assertEquals("null-qrs", ((String) null)._mapNullable(value -> value + "-qrs"));
+        assertNull(((String) null)._mapNullable(value -> null));
+    }
+
+    @Test
+    void _mapNullable_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "random"._mapNullable(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._mapNullable(null));
+    }
+
+    @Test
+    void _filter() {
+        assertEquals("foo", "foo"._filter(value -> true));
+        assertNull("bar"._filter(value -> false));
+        assertNull(((String) null)._filter(Assertions::fail));
+    }
+
+    @Test
+    void _filter_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "some_value_here"._filter(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._filter(null));
+    }
+
+    @Test
+    void _filterNullable() {
+        assertEquals("foo", "foo"._filterNullable(value -> true));
+        assertNull("bar"._filterNullable(value -> false));
+        assertNull(((String) null)._filterNullable(value -> true));
+        assertNull(((String) null)._filterNullable(value -> false));
+    }
+
+    @Test
+    void _filterNullable_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "some_value_here"._filterNullable(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._filterNullable(null));
+    }
+
+    @Test
+    void _orElseThrow() {
+        assertEquals("foo", "foo"._orElseThrow(Assertions::fail));
+        assertThrows(SuccessException.class, () -> ((String) null)._orElseThrow(SuccessException::new));
+    }
+
+
+    @Test
+    void _orElseThrow_throwsNPEOnNullLambda() {
+        assertThrows(NullPointerException.class, () -> "some_value_here"._orElseThrow(null));
+        assertThrows(NullPointerException.class, () -> ((String) null)._orElseThrow(null));
+    }
+
+    @Test
+    void _orElseThrow_throwsNPEOnNullLambdaValue() {
+        assertThrows(NullPointerException.class, () -> ((String) null)._orElseThrow(() -> null));
+    }
+
+
+    @Test
+    void _stream() {
+        assertArrayEquals(new String[]{"value"}, "value"._stream().toArray(String[]::new));
+        assertArrayEquals(EMPTY_STRING_ARRAY, ((String) null)._stream().toArray(String[]::new));
+    }
+
+    @Test
+    void _streamOfNullable() {
+        assertArrayEquals(new String[]{"value"}, "value"._streamOfNullable().toArray(String[]::new));
+        assertArrayEquals(new String[]{null}, ((String) null)._streamOfNullable().toArray(String[]::new));
+    }
+
+    // this class cannot be local
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    private static final class SuccessException extends Exception {}
+}
