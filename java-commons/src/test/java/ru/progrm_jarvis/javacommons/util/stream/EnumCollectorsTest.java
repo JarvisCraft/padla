3dba601f9b30ee29ae222cb0215b0a7f254e1bd1
Petr Portnov <gh@progrm-jarvis.ru>
2021-05-08 19:56:05 +0300

    test: enhance EnumCollectorsTest

--- java-commons/src/test/java/ru/progrm_jarvis/javacommons/util/stream/EnumCollectorsTest.java
+++ java-commons/src/test/java/ru/progrm_jarvis/javacommons/util/stream/EnumCollectorsTest.java
@@ -2,24 +2,27 @@ package ru.progrm_jarvis.javacommons.util.stream;
 
 import lombok.NonNull;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.progrm_jarvis.javacommons.collection.MapFiller;
 
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class EnumCollectorsTest {
 
     static @NotNull Stream<@NotNull Arguments> enumNameMaps() {
         return Stream.of(
+                arguments(
+                        Stream.empty(),
+                        Collections.emptyMap()
+                ),
                 arguments(
                         Stream.of("A", "B", "C"),
                         MapFiller.from(new EnumMap<>(MyEnum.class))
@@ -34,6 +37,10 @@ class EnumCollectorsTest {
     static @NotNull Stream<@NotNull Arguments> enumMaps() {
         return Stream.of(
                 arguments(
+                        Stream.empty(),
+                        Collections.emptyMap()
+                ),
+                arguments(
                         Stream.of(MyEnum.A, MyEnum.B, MyEnum.C),
                         MapFiller.from(new EnumMap<>(MyEnum.class))
                                 .put(MyEnum.A, "<A>")
@@ -44,10 +51,23 @@ class EnumCollectorsTest {
         );
     }
 
+    static @NotNull Stream<@NotNull Arguments> enumNameSets() {
+        return Stream.of(
+                arguments(
+                        Stream.empty(),
+                        Collections.emptySet()
+                ),
+                arguments(
+                        Stream.of("A", "B", "C"),
+                        EnumSet.of(MyEnum.A, MyEnum.B, MyEnum.C)
+                )
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("enumNameMaps")
-    void toEnumMapFull(final @NonNull Stream<@NotNull String> enumNames,
-                       final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
+    void toEnumMap_full(final @NonNull Stream<@NotNull String> enumNames,
+                        final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
         assertThat(
                 enumNames.collect(EnumCollectors.toEnumMap(
                         MyEnum.class, MyEnum::valueOf,
@@ -63,8 +83,8 @@ class EnumCollectorsTest {
 
     @ParameterizedTest
     @MethodSource("enumNameMaps")
-    void toEnumMapNoMerger(final @NonNull Stream<@NotNull String> enumNames,
-                           final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
+    void toEnumMap_noMerger(final @NonNull Stream<@NotNull String> enumNames,
+                            final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
         assertThat(
                 enumNames.collect(EnumCollectors.toEnumMap(
                         MyEnum.class, MyEnum::valueOf,
@@ -76,8 +96,8 @@ class EnumCollectorsTest {
 
     @ParameterizedTest
     @MethodSource("enumMaps")
-    void toEnumMapNoKeyMapper(final @NonNull Stream<@NotNull MyEnum> enumNames,
-                              final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
+    void toEnumMap_noKeyMapper(final @NonNull Stream<@NotNull MyEnum> enumNames,
+                               final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
         assertThat(
                 enumNames.collect(EnumCollectors.toEnumMap(
                         MyEnum.class,
@@ -93,14 +113,24 @@ class EnumCollectorsTest {
 
     @ParameterizedTest
     @MethodSource("enumMaps")
-    void toEnumMapNoKeyMapperNoMerger(final @NonNull Stream<@NotNull MyEnum> enumNames,
-                                      final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
+    void toEnumMap_noKeyMapperNoMerger(final @NonNull Stream<@NotNull MyEnum> enumNames,
+                                       final @NonNull Map<@NotNull MyEnum, @NotNull Integer> result) {
         assertThat(
                 enumNames.collect(EnumCollectors.toEnumMap(
                         MyEnum.class,
                         name -> "<" + name + ">"
                 )).entrySet(),
                 containsInAnyOrder(result.entrySet().toArray())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("enumNameSets")
+    void toEnumSet(final @NonNull Stream<@NotNull String> enumNames,
+                   final @NonNull Set<@NotNull MyEnum> result) {
+        assertEquals(
+                enumNames.map(MyEnum::valueOf).collect(EnumCollectors.toEnumSet(MyEnum.class)),
+                result
         );
     }
 
