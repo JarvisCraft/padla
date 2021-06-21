face728c8b1f3963d8d97151d2abcd89df1e9547
Petr Portnov <gh@progrm-jarvis.ru>
2021-06-21 12:25:57 +0300

    feat: add @Any

---
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/annotation/Any.java
@@ -0,0 +1,15 @@
+package ru.progrm_jarvis.javacommons.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Target;
+
+/**
+ * Marker indicating that the annotated type parameter does not matter
+ * thus any unchecked casts from this type to <b>any</b> other are safe and do not lead to unspecified behaviour.
+ */
+@Inherited
+@Documented
+@Target(ElementType.TYPE_PARAMETER)
+public @interface Any {}
