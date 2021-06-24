ac156a6377be73f1a863c435081e61812d6c0827
Petr Portnov <gh@progrm-jarvis.ru>
2021-06-25 02:26:09 +0300

    refactor: implement a simpler `CompletableFutures#bothOf(..)`
    Co-authored-by: Zabelov <zabelov@zabelix.ru>

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/concurrent/CompletableFutures.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/concurrent/CompletableFutures.java
@@ -2,7 +2,7 @@ package ru.progrm_jarvis.javacommons.util.concurrent;
 
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
-import lombok.val;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import ru.progrm_jarvis.javacommons.object.Pair;
 
@@ -19,20 +19,18 @@ public class CompletableFutures {
      * Returns the completable future whose result is a pair of the provided future's results if both of them complete.
      * The future completes exceptionally if any of the given futures completes exceptionally.
      *
-     * @param first first futures
-     * @param second second futures
+     * @param first first future
+     * @param second second future
      * @param <F> the result type of the first future
      * @param <S> the result type of the second future
      * @return a new completable future that is completed when both of the given futures complete
      *
      * @see CompletableFuture#allOf(CompletableFuture[]) var-arg untyped equivalent
      */
-    public @NotNull <F, S> CompletableFuture<@NotNull Pair<F, S>> bothOf(final @NonNull CompletableFuture<F> first,
-                                                                         final @NonNull CompletableFuture<S> second) {
-        val firstResult = new AtomicReference<F>();
-        val secondResult = new AtomicReference<S>();
-
-        return CompletableFuture.allOf(first.thenAccept(firstResult::set), second.thenAccept(secondResult::set))
-                .thenApply(unused -> Pair.of(firstResult.get(), secondResult.get()));
+    public @NotNull <F, S> CompletableFuture<@NotNull Pair<F, S>> bothOf(
+            final @NonNull CompletableFuture<F> first,
+            @SuppressWarnings("TypeMayBeWeakened" /* for API stability */) final @NonNull CompletableFuture<S> second
+    ) {
+        return first.thenCombine(second, Pair::of);
     }
 }
