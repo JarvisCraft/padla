06295343d897e862f2d7af375a81653445fb163b
Petr Portnov <gh@progrm-jarvis.ru>
2021-09-18 03:40:09 +0300

    feat(java-commons): add `Runnables#stateful(..)`

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/Runnables.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/Runnables.java
@@ -1,7 +1,14 @@
 package ru.progrm_jarvis.javacommons.util.function;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Utilities related to {@link Runnable runnables}.
@@ -16,5 +23,82 @@ public class Runnables {
      */
     public @NotNull Runnable none() {
         return () -> {};
+    }
+
+    /**
+     * Creates a stateful runnable which stores its state mutably.
+     *
+     * @param initialState initial state of the created runnable
+     * @param handler handler invoked on each call to {@link Runnable#run()} with the state applied to it
+     * @param <T> type of stored state
+     * @return created stateful runnable
+     */
+    public <T> @NotNull Runnable stateful(final @NotNull T initialState,
+                                          final @NotNull Consumer<? super T> handler) {
+        return new StatefulImmutableRunnable<>(handler, initialState);
+    }
+
+    /**
+     * Creates a stateful runnable which stores its state immutably.
+     *
+     * @param initialState initial state of the created runnable
+     * @param handler handler invoked on each call to {@link Runnable#run()} with the state applied to it
+     * providing the new state
+     * @param <T> type of stored state
+     * @return created stateful runnable
+     */
+    public <T> @NotNull Runnable stateful(final @NotNull T initialState,
+                                          final @NotNull Function<? super T, ? extends T> handler) {
+        return new StatefulMutableRunnable<>(handler, initialState);
+    }
+
+    /**
+     * Stateful {@link Runnable runnable} which stores its state immutably.
+     *
+     * @param <T> type of stored state
+     */
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class StatefulImmutableRunnable<T> implements Runnable {
+
+        /**
+         * Handler invoked on each call to {@link #run()} with the state applied to it
+         */
+        @NotNull Consumer<? super T> handler;
+
+        /**
+         * State of this runnable
+         */
+        T state;
+
+        @Override
+        public void run() {
+            handler.accept(state);
+        }
+    }
+
+    /**
+     * Stateful {@link Runnable runnable} which stores its state mutably.
+     *
+     * @param <T> type of stored state
+     */
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class StatefulMutableRunnable<T> implements Runnable {
+
+        /**
+         * Handler invoked on each call to {@link #run()} with the state applied to it providing the new state
+         */
+        @NotNull Function<? super T, ? extends T> handler;
+
+        /**
+         * State of this runnable
+         */
+        @NonFinal T state;
+
+        @Override
+        public void run() {
+            state = handler.apply(state);
+        }
     }
 }
