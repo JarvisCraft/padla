4086d0fd44862397f7605736e66112632063ef1c
Petr Portnov <gh@progrm-jarvis.ru>
2021-09-18 04:10:30 +0300

    fix(java-commons): add common primitive specializations for `Runnables#stateful(..)`

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/Runnables.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/Runnables.java
@@ -8,8 +8,7 @@ import lombok.experimental.NonFinal;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.*;
 
 /**
  * Utilities related to {@link Runnable runnables}.
@@ -38,7 +37,7 @@ public class Runnables {
      */
     public <T> @NotNull Runnable stateful(final T initialState,
                                           final @NonNull Consumer<? super T> handler) {
-        return new StatefulImmutableRunnable<>(handler, initialState);
+        return new StatefulMutableImRunnable<>(handler, initialState);
     }
 
     /**
@@ -58,13 +57,58 @@ public class Runnables {
     }
 
     /**
+     * Creates a stateful runnable which stores its state immutably.
+     *
+     * @param initialState initial state of the created runnable
+     * @param handler handler invoked on each call to {@link Runnable#run()} with the state applied to it
+     * providing the new state
+     * @return created stateful runnable
+     *
+     * @throws NullPointerException if {@code handler} is {@code null}
+     */
+    public @NotNull Runnable stateful(final int initialState,
+                                      final @NonNull IntUnaryOperator handler) {
+        return new StatefulMutableIntRunnable(handler, initialState);
+    }
+
+    /**
+     * Creates a stateful runnable which stores its state immutably.
+     *
+     * @param initialState initial state of the created runnable
+     * @param handler handler invoked on each call to {@link Runnable#run()} with the state applied to it
+     * providing the new state
+     * @return created stateful runnable
+     *
+     * @throws NullPointerException if {@code handler} is {@code null}
+     */
+    public @NotNull Runnable stateful(final long initialState,
+                                      final @NonNull LongUnaryOperator handler) {
+        return new StatefulMutableLongRunnable(handler, initialState);
+    }
+
+    /**
+     * Creates a stateful runnable which stores its state immutably.
+     *
+     * @param initialState initial state of the created runnable
+     * @param handler handler invoked on each call to {@link Runnable#run()} with the state applied to it
+     * providing the new state
+     * @return created stateful runnable
+     *
+     * @throws NullPointerException if {@code handler} is {@code null}
+     */
+    public @NotNull Runnable stateful(final double initialState,
+                                      final @NonNull DoubleUnaryOperator handler) {
+        return new StatefulMutableDoubleRunnable(handler, initialState);
+    }
+
+    /**
      * Stateful {@link Runnable runnable} which stores its state immutably.
      *
      * @param <T> type of stored state
      */
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-    private static final class StatefulImmutableRunnable<T> implements Runnable {
+    private static final class StatefulMutableImRunnable<T> implements Runnable {
 
         /**
          * Handler invoked on each call to {@link #run()} with the state applied to it
@@ -104,6 +148,75 @@ public class Runnables {
         @Override
         public void run() {
             state = handler.apply(state);
+        }
+    }
+
+    /**
+     * Specialization of {@link StatefulMutableRunnable} for {@code int}s which stores its state mutably.
+     */
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class StatefulMutableIntRunnable implements Runnable {
+
+        /**
+         * Handler invoked on each call to {@link #run()} with the state applied to it providing the new state
+         */
+        @NotNull IntUnaryOperator handler;
+
+        /**
+         * State of this runnable
+         */
+        @NonFinal int state;
+
+        @Override
+        public void run() {
+            state = handler.applyAsInt(state);
+        }
+    }
+
+    /**
+     * Specialization of {@link StatefulMutableRunnable} for {@code long}s which stores its state mutably.
+     */
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class StatefulMutableLongRunnable implements Runnable {
+
+        /**
+         * Handler invoked on each call to {@link #run()} with the state applied to it providing the new state
+         */
+        @NotNull LongUnaryOperator handler;
+
+        /**
+         * State of this runnable
+         */
+        @NonFinal long state;
+
+        @Override
+        public void run() {
+            state = handler.applyAsLong(state);
+        }
+    }
+
+    /**
+     * Specialization of {@link StatefulMutableRunnable} for {@code double}s which stores its state mutably.
+     */
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    private static final class StatefulMutableDoubleRunnable implements Runnable {
+
+        /**
+         * Handler invoked on each call to {@link #run()} with the state applied to it providing the new state
+         */
+        @NotNull DoubleUnaryOperator handler;
+
+        /**
+         * State of this runnable
+         */
+        @NonFinal double state;
+
+        @Override
+        public void run() {
+            state = handler.applyAsDouble(state);
         }
     }
 }
