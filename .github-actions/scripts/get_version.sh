4ec2ec3ad8264a3cf331826a23115cdd503db993
Petr Portnov <gh@progrm-jarvis.ru>
2021-09-18 18:00:55 +0300

    ci: simplify deployment and deprecate `Github Package Registry` deployment

--- .github-actions/scripts/get_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This should be called from repository root
-
-# shellcheck disable=SC2016
-# this is intentional as it is the value passed to Maven
-mvn -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec --quiet
