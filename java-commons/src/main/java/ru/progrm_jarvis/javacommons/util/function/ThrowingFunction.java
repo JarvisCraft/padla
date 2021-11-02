789c409da8cd6acf0710d7554bdd55f71685e1ef
Petr Portnov <gh@progrm-jarvis.ru>
2021-11-03 00:29:01 +0300

    refactor(java-commons): use more consistent naming for `Throwing*` functional interfaces

--- java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/ThrowingFunction.java
+++ java-commons/src/main/java/ru/progrm_jarvis/javacommons/util/function/ThrowingFunction.java
@@ -21,11 +21,25 @@ public interface ThrowingFunction<T, R, X extends Throwable> extends Function<T,
      * @return the function result
      * @throws X if an exception happens
      */
-    R invoke(T t) throws X;
+    R applyChecked(T t) throws X;
+
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param t the function argument
+     * @return the function result
+     * @throws X if an exception happens
+     *
+     * @deprecated in favor of {@link #applyChecked(Object)}
+     */
+    @Deprecated
+    default R invoke(final T t) throws X {
+        return applyChecked(t);
+    }
 
     @Override
     @SneakyThrows
     default R apply(final T t) {
-        return invoke(t);
+        return applyChecked(t);
     }
 }
