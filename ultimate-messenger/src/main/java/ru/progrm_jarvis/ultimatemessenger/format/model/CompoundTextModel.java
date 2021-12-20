f85b2a85666e57394e0abd3e2fdabe7079f6002f
Petr Portnov <gh@progrm-jarvis.ru>
2021-12-20 20:14:19 +0300

    refactor: implement `CompoundTextModel#write(..)`

--- ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/CompoundTextModel.java
+++ ultimate-messenger/src/main/java/ru/progrm_jarvis/ultimatemessenger/format/model/CompoundTextModel.java
@@ -1,7 +1,13 @@
 package ru.progrm_jarvis.ultimatemessenger.format.model;
 
+import lombok.NonNull;
+import lombok.val;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,5 +23,20 @@ public interface CompoundTextModel<T> extends TextModel<T>, List<TextModel<T>> {
         return stream()
                 .map(element -> element.getText(target))
                 .collect(Collectors.joining());
+    }
+
+    @Override
+    default void write(final @NonNull DataOutputStream output, final T target) throws IOException {
+        for (val element : this) element.write(output, target);
+    }
+
+    @Override
+    default void write(final @NonNull Writer output, final T target) throws IOException {
+        for (val element : this) element.write(output, target);
+    }
+
+    @Override
+    default void write(final @NonNull PrintWriter output, final T target) {
+        for (val element : this) element.write(output, target);
     }
 }
